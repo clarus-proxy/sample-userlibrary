a958e347d0bf0febfc77b72f82b40a1e614b4900
Diego Rivera <diego.rivera@montimage.com>
2017-08-07 11:09:05 +0200

    Modifications to comply with CLARUS formatting agreement.

--- src/main/java/eu/clarussecure/proxy/access/CLARUSUserOperations.java
+++ src/main/java/eu/clarussecure/proxy/access/CLARUSUserOperations.java
@@ -11,64 +11,64 @@ import java.util.logging.Logger;
 import org.bson.Document;
 
 public class CLARUSUserOperations {
-	// Singleton implementation
-	private static CLARUSUserOperations instance = null;
-	private int instancesNumber;
-	protected MongoDatabase db;
-	protected MongoClient mongoClient;
-	
-	protected CLARUSUserOperations(){
+    // Singleton implementation
+    private static CLARUSUserOperations instance = null;
+    private int instancesNumber;
+    protected MongoDatabase db;
+    protected MongoClient mongoClient;
+
+    protected CLARUSUserOperations() {
         // Correctly configure the log level
-        Logger mongoLogger = Logger.getLogger( "org.mongodb.driver" );
-        mongoLogger.setLevel(Level.SEVERE); 
-		// Create a new client connecting to "localhost" on port 
-		this.mongoClient = new MongoClient("localhost", 27017);
+        Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
+        mongoLogger.setLevel(Level.SEVERE);
+        // Create a new client connecting to "localhost" on port 
+        this.mongoClient = new MongoClient("localhost", 27017);
 
-		// Get the database (will be created if not present)
-		this.db = mongoClient.getDatabase("CLARUS");
+        // Get the database (will be created if not present)
+        this.db = mongoClient.getDatabase("CLARUS");
 
-		this.instancesNumber++;
-	}
+        this.instancesNumber++;
+    }
 
-	public void deleteInstance(){
-		this.instancesNumber--;
+    public void deleteInstance() {
+        this.instancesNumber--;
 
-		if(this.instancesNumber <= 0){
-			this.mongoClient.close();
-			CLARUSUserOperations.instance = null;
-		}
-	}
+        if (this.instancesNumber <= 0) {
+            this.mongoClient.close();
+            CLARUSUserOperations.instance = null;
+        }
+    }
 
-	public static CLARUSUserOperations getInstance(){
-		if (CLARUSUserOperations.instance == null)
-			CLARUSUserOperations.instance = new CLARUSUserOperations();
-		return CLARUSUserOperations.instance;
-	}
+    public static CLARUSUserOperations getInstance() {
+        if (CLARUSUserOperations.instance == null)
+            CLARUSUserOperations.instance = new CLARUSUserOperations();
+        return CLARUSUserOperations.instance;
+    }
 
-	public boolean addUser(String username, String password, String role) {
-		MongoCollection<Document> collection = db.getCollection("users");
-		
-		Document doc = new Document("username", username);
-		// FIXME - Only admin user are added in the meantime
-		doc.append("profile", role);
-		doc.append("password", password);
-		
-		collection.insertOne(doc);
-		
-		return true;
-	}
-	
-	public Set<String> listUsers(){
-		MongoCollection<Document> collection = db.getCollection("users");
-		Set<String> result = new HashSet<>();
-		
-		// Iterate over the results
-		MongoCursor<Document> cursor = collection.find().iterator();
-		
-		while(cursor.hasNext()){
-			result.add(cursor.next().getString("username"));
-		}
-		
-		return result;
-	}
+    public boolean addUser(String username, String password, String role) {
+        MongoCollection<Document> collection = db.getCollection("users");
+
+        Document doc = new Document("username", username);
+        // FIXME - Only admin user are added in the meantime
+        doc.append("profile", role);
+        doc.append("password", password);
+
+        collection.insertOne(doc);
+
+        return true;
+    }
+
+    public Set<String> listUsers() {
+        MongoCollection<Document> collection = db.getCollection("users");
+        Set<String> result = new HashSet<>();
+
+        // Iterate over the results
+        MongoCursor<Document> cursor = collection.find().iterator();
+
+        while (cursor.hasNext()) {
+            result.add(cursor.next().getString("username"));
+        }
+
+        return result;
+    }
 }
--- src/main/java/eu/clarussecure/proxy/access/SimpleMongoUserAccess.java
+++ src/main/java/eu/clarussecure/proxy/access/SimpleMongoUserAccess.java
@@ -8,67 +8,67 @@ import java.util.logging.Logger;
 import org.bson.Document;
 
 public class SimpleMongoUserAccess extends CLARUSUserOperations implements CLARUSAccess {
-	private static SimpleMongoUserAccess instance = null;
-	private int instancesNumber;
-	
-	private SimpleMongoUserAccess(){
+    private static SimpleMongoUserAccess instance = null;
+    private int instancesNumber;
+
+    private SimpleMongoUserAccess() {
         // Correctly configure the log level
-        Logger mongoLogger = Logger.getLogger( "org.mongodb.driver" );
-        mongoLogger.setLevel(Level.SEVERE); 
-		// Create a new client connecting to "localhost" on port 
-		this.mongoClient = new MongoClient("localhost", 27017);
+        Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
+        mongoLogger.setLevel(Level.SEVERE);
+        // Create a new client connecting to "localhost" on port 
+        this.mongoClient = new MongoClient("localhost", 27017);
 
-		// Get the database (will be created if not present)
-		this.db = mongoClient.getDatabase("CLARUS");
+        // Get the database (will be created if not present)
+        this.db = mongoClient.getDatabase("CLARUS");
 
-		this.instancesNumber++;
-	}
-
-    @Override
-	public void deleteInstance(){
-		this.instancesNumber--;
-
-		if(this.instancesNumber <= 0){
-			this.mongoClient.close();
-			SimpleMongoUserAccess.instance = null;
-		}
-	}
-
-	public static SimpleMongoUserAccess getInstance(){
-		if (SimpleMongoUserAccess.instance == null)
-			SimpleMongoUserAccess.instance = new SimpleMongoUserAccess();
-		return SimpleMongoUserAccess.instance;
-	}
+        this.instancesNumber++;
+    }
 
     @Override
-	public boolean authenticate(String username, String password) {
-		// This method assumes the username exists. If not, it will fail.
-		// It is encourages to use this.identify(username= before this method.
-		MongoCollection<Document> collection = db.getCollection("users");
-		
-		// Search the user entry
-		Document user = collection.find(eq("username", username)).first();
+    public void deleteInstance() {
+        this.instancesNumber--;
+
+        if (this.instancesNumber <= 0) {
+            this.mongoClient.close();
+            SimpleMongoUserAccess.instance = null;
+        }
+    }
+
+    public static SimpleMongoUserAccess getInstance() {
+        if (SimpleMongoUserAccess.instance == null)
+            SimpleMongoUserAccess.instance = new SimpleMongoUserAccess();
+        return SimpleMongoUserAccess.instance;
+    }
+
+    @Override
+    public boolean authenticate(String username, String password) {
+        // This method assumes the username exists. If not, it will fail.
+        // It is encourages to use this.identify(username= before this method.
+        MongoCollection<Document> collection = db.getCollection("users");
+
+        // Search the user entry
+        Document user = collection.find(eq("username", username)).first();
         // Compare the passwords
-		
-		return user.getString("password").equals(password);
-	}
+
+        return user.getString("password").equals(password);
+    }
 
     @Override
-	public boolean identify(String username) {
-		MongoCollection<Document> collection = db.getCollection("users");
-		
-		// Search all the entries with the given username
-		long users = collection.count(eq("username", username));
-		
-		// Return true iff only one was found
-		return users == 1;
-	}
-    
-    public String userProfile(String username){
-		MongoCollection<Document> collection = db.getCollection("users");
-		
-		// Search the user entry
-		Document user = collection.find(eq("username", username)).first();
+    public boolean identify(String username) {
+        MongoCollection<Document> collection = db.getCollection("users");
+
+        // Search all the entries with the given username
+        long users = collection.count(eq("username", username));
+
+        // Return true iff only one was found
+        return users == 1;
+    }
+
+    public String userProfile(String username) {
+        MongoCollection<Document> collection = db.getCollection("users");
+
+        // Search the user entry
+        Document user = collection.find(eq("username", username)).first();
         return user.getString("profile");
     }
 }
